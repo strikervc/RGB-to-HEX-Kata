9315f3f81dee171808748023dac5da1a0ffeb6b7
Victor Valdez <victor334_@hotmail.com>
2020-12-16 09:32:34 -0400

    rgb_to_hex method added and a new test scenario added

--- test_rgb_to_hex.py
+++ test_rgb_to_hex.py
@@ -1,7 +1,12 @@
 import unittest
 from rgb_to_hex import converter
 class testRGBtoHEX (unittest.TestCase):
-    def test_rgb_to_hex(self):
+    def test_rgb_to_hex_01(self):
         value = converter()
         result = "7bff8e"
         self.assertEqual(result, value.rgb_to_hex((123, 255, 142)))
+
+    def test_rgb_to_hex_02(self):
+        value = converter()
+        result = "86f584"
+        self.assertEqual(result, value.rgb_to_hex((134, 245, 132)))    
