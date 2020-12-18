fcc4f10b7f1b7814d0cc04fbd3e9f061dab0f20a
Victor Valdez <victor334_@hotmail.com>
2020-12-18 19:35:04 -0400

    test_rgb_to_hex_02 and test_rgb_to_hex_03 added

--- test_rgb_to_hex.py
+++ test_rgb_to_hex.py
@@ -5,3 +5,13 @@ class testCases (unittest.TestCase):
         value = converter()
         result = "6f6f6f"
         self.assertEqual(result, value.rgb_to_hex((111, 111, 111)))
+    
+    def test_rgb_to_hex_02(self):
+        value = converter()
+        result = "de6fde"
+        self.assertEqual(result, value.rgb_to_hex((222, 111, 222)))
+    
+    def test_rgb_to_hex_03(self):
+        value = converter()
+        result = "7a6f7a"
+        self.assertEqual(result, value.rgb_to_hex((122, 111, 122)))
